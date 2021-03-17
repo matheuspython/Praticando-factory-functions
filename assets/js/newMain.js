40f5cca2397c226178659865f7863dbaa10e1ad1
matheus souza <ruivorobado@gmail.com>
2021-03-17 17:52:23 -0300

    added functionnality that places the clicked value in the input

--- assets/js/newMain.js
+++ assets/js/newMain.js
@@ -1,16 +1,26 @@
 function criaCalculadora(){
   return {
     display: document.querySelector('.display'),
-    
-
-
-
 
 
     inicia(){
-
+      this.cliqueBotoes()
     },
     
+    cliqueBotoes() {
+      document.addEventListener('click', function(e) {
+        const el = e.target;
+      
+        if(el.classList.contains('btn-num')) {
+          this.btnParaDisplay(el.innerText)
+        }
+      }.bind(this));//bind sendo usado para tirar o this do document e colocar na instancia do obj
+    },
+
+    btnParaDisplay(valor) {
+      this.display.value += valor
+    },
+
   };
 }
 
