ee74367c043d1d3e47b26793c2a50b42da50b1bc
matheus souza <ruivorobado@gmail.com>
2021-03-16 23:16:53 -0300

    adicionando arquivos iniciais

---
+++ assets/js/newMain.js
@@ -0,0 +1,18 @@
+function criaCalculadora(){
+  return {
+    display: document.querySelector('.display'),
+    
+
+
+
+
+
+    inicia(){
+
+    },
+    
+  };
+}
+
+const calculadora = criaCalculadora()
+calculadora.inicia()
