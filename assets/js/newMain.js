9e577cc1a0900cf1c49291df571874def2bc1f2d
matheus souza <ruivorobado@gmail.com>
2021-03-17 18:02:02 -0300

    added functionality to clear the display

--- assets/js/newMain.js
+++ assets/js/newMain.js
@@ -2,19 +2,27 @@ function criaCalculadora(){
   return {
     display: document.querySelector('.display'),
 
+    clearDisplay(){
+      this.display.value = ''
+    },
 
     inicia(){
       this.cliqueBotoes()
+      
     },
     
     cliqueBotoes() {
-      document.addEventListener('click', function(e) {
+      document.addEventListener('click', e => {
         const el = e.target;
       
         if(el.classList.contains('btn-num')) {
           this.btnParaDisplay(el.innerText)
         }
-      }.bind(this));//bind sendo usado para tirar o this do document e colocar na instancia do obj
+
+        if(el.classList.contains('btn-clear')){
+          this.clearDisplay()
+        }
+      });
     },
 
     btnParaDisplay(valor) {
